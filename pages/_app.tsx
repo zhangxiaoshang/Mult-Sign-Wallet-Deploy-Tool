8273f781c48b8c9e956cfa73a7760e0c104a722f
bolingboling <zhangxiaoshang66@163.com>
2023-01-10 14:53:49 +0800

    deploy contract

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { DAppProvider, Config, BSCTestnet, BSC } from "@usedapp/core";
+import { getDefaultProvider } from "ethers";
+
+const config: Config = {
+  readOnlyChainId: BSCTestnet.chainId,
+  readOnlyUrls: {
+    [BSCTestnet.chainId]: getDefaultProvider(
+      process.env.NEXT_PUBLIC_BSC_TESTNET_RPC
+    ),
+    // [BSC.chainId]: getDefaultProvider(process.env.NEXT_PUBLIC_BSC_RPC),
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DAppProvider config={config}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  );
 }
